805753d8599d91f5081ff484c7f441a200349dd6
Anassess7 <anassess7@icloud.com>
2025-06-06 14:48:46 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: '4f9d-81-192-179-33.ngrok-free.app',
-            protocol: 'wss',
+    build: {
+        outDir: 'public/build',
+        rollupOptions: {
+          input: 'resources/js/app.js',
         },
-    },
+      },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
